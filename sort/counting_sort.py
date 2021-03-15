45f933041379b208d3059e27ec8df8ae430de279
rlaehdgnsc <rlaehdgnsc@naver.com>
2021-03-15 11:19:35 +0900

    counting sort

--- sort/counting_sort.py
+++ sort/counting_sort.py
@@ -1,1 +1,11 @@
-# Working on..
+array = [7,5,9,0,3,1,6,2,9,1,4,8,0,5,2]
+
+cnt_sort = [0]*(max(array)+1)
+
+for i in array:
+    cnt_sort[i]+=1
+
+for idx,val in enumerate(cnt_sort):
+    for i in range(val):
+        print(idx ,end=' ')
+    
